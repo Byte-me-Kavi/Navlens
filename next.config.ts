1ee9966c0f3c018b75d4189a4edf36ef3bf4baed
Kavisha Trishan <kaveeshatmdss@gmail.com>
2025-11-18 10:12:08 +0530

    Update puppeteer-core and @sparticuz/chromium to latest versions for Vercel compatibility

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ['puppeteer-core', '@sparticuz/chromium', 'puppeteer'],
+  serverExternalPackages: ['@sparticuz/chromium'],
   turbopack: {},
   async headers() {
     return [
